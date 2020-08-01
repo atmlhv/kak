d36c77c1552233a935bfdb319216b89e1973791b
atmlhv <nandeglaywww@gmail.com>
2020-08-02 04:48:24 +0900

    とりあえずテロップの配置とそれっぽい位置にテキスト生成するだけ

---
+++ Assets/Scripts/TelopFrame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TelopFrame : TelopItem
+{
+    //[SerializeField]
+    //Text time;
+    //TimeController timeController;
+
+    public void Initialize(Vector2 t_position)
+    {
+        //timeController = FindObjectOfType<TimeController>();
+        transform.localPosition = t_position;
+    }
+
+    private void Update()
+    {
+      //  time.text = Utility.SecondToText(timeController.currentSecond, true);
+    }
+}
---
+++ Assets/Scripts/TelopText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TelopText : MonoBehaviour
+{
+    [SerializeField]
+    Text teloptext;
+
+    public void Initialize(Vector2 t_position) //float t_time, string t_lapName, Vector2 t_position, Vector2 t_sizeDelta)
+    {
+        transform.localPosition = t_position;
+        //baseImage.rectTransform.sizeDelta = t_sizeDelta;
+
+        teloptext.text = "testtttttttttttttt";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
